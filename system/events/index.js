ece964eb1e580a5e7b059610c3ff1cca7c0e1572
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2023-10-11 01:22:51 +0300

    Init project

---
+++ system/events/index.js
@@ -0,0 +1,9 @@
+function getEventInformation(eventID){ 
+    // Call some function,
+    const eventName = "hello";
+    return eventName;
+}
+
+module.exports = {
+    getEventInformation,
+}
---
+++ system/index.js
@@ -0,0 +1,18 @@
+const eventsModule = require('./events');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {    
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    
+    const eventData = eventsModule.getEventInformation(0);
+    console.log("Data from events module: "+ eventData)
+});
+
+
