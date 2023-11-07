ddf4168e95a4cc2f81f3410499d2cbf448ce23d0
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2023-11-07 17:23:36 +0200

    rename register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RegistrationPage from './routes/login'
+import RegistrationPage from './routes/register'
 function App() {
   return (
     <BrowserRouter>
