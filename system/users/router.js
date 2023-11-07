ece9567946c08c53b0aefa658b4713a70e47210b
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2023-11-07 17:23:25 +0200

    rename to userController

--- system/users/router.js
+++ system/users/router.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const usersService = require("./")
-const {SUCCESS, FAIL} = require('./status/statusConstants')
+const express = require('express');
+const router = express.Router();
+const userControlller = require("./userController");
+const {SUCCESS, FAIL} = require('./status/statusConstants');
 
 router.post('/register', async (req, res) => {   
     // Handle request.
     const username = req.body.username;   
     const password = req.body.password; 
-    const statusObject = await usersService.registerUser(username, password);
+    const statusObject = await userControlller.registerUser(username, password);
    
     // Send back the status and message.
     res.status(statusObject.httpStatus).send({message: statusObject.message});
@@ -17,7 +17,7 @@ router.post('/login', async (req, res) => {
     // Handle request.
     const username = req.body.username;   
     const password = req.body.password; 
-    const statusObject = await usersService.login(username, password);
+    const statusObject = await userControlller.login(username, password);
 
     // Send back the status and message.
     res.status(statusObject.httpStatus).send({message: statusObject.message});
