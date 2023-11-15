2e6167d7f91d6a724c48e80b43c13ea8970cbeae
Tanjim Pranto <tanjim.outcast@gmail.com>
2023-11-15 23:24:03 +0200

    add login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegistrationPage from './routes/register'
+import Login from './routes/Login.jsx'
+import Signup from './routes/Signup.jsx'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<RegistrationPage />}>
-        </Route>
+        <Route path="/register" element={<RegistrationPage />}></Route>
+		    <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>  );
 }
