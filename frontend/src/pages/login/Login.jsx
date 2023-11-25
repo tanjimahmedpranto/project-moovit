1810efa53a12edbc59845dc25c8b532bdebdcb13
Tanjim Pranto <tanjim.outcast@gmail.com>
2023-11-25 03:12:03 +0200

    validation, submission logic login, fetch request

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/FormBase.css";
 import { Link } from "react-router-dom";
 import { default as logo } from "../../assets/muuvitLogo.svg";
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
-import "../../styles/FormBase.css";
+import Cookies from "js-cookie";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  const validateEmail = (email) => {
+    const re =
+      /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 8;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setValidated(true);
+
+    if (validateEmail(email) && validatePassword(password)) {
+      try {
+        const response = await fetch("YOUR_SERVER_ENDPOINT/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          // Assuming the JWT is in data.token
+          Cookies.set("jwt", data.token); // Save the JWT in a cookie
+          console.log("Login successful");
+          // Redirect or perform other actions on successful login
+        } else {
+          console.error("Login failed:", data.message);
+          // Handle login failure (show error message, etc.)
+        }
+      } catch (error) {
+        console.error("There was an error logging in:", error);
+        // Handle network errors, server down, etc.
+      }
+    }
+  };
+
   return (
     <div>
       <Container>
@@ -39,13 +86,27 @@ export default function Login() {
                   <h3>Sign In</h3>
                   <br />
                   <div className="mb-3">
-                    <Form>
+                    <Form
+                      noValidate
+                      validated={validated}
+                      onSubmit={handleSubmit}
+                    >
                       {/* Email address logic begin */}
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="text-center">
                           Email address
                         </Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control
+                          required
+                          type="email"
+                          placeholder="Enter email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          isInvalid={validated && !validateEmail(email)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Please enter a valid email address.
+                        </Form.Control.Feedback>
                       </Form.Group>
 
                       {/* Password logic begin */}
@@ -54,7 +115,18 @@ export default function Login() {
                         controlId="formBasicPassword"
                       >
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control
+                          required
+                          type="password"
+                          placeholder="Password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          isInvalid={validated && !validatePassword(password)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Please enter a password that is at least 8 characters
+                          long.
+                        </Form.Control.Feedback>
                       </Form.Group>
 
                       <Form.Group
