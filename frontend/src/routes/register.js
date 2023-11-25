7c6d945636c03115af7a4fb8932d8cd46b9acab2
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2023-11-25 17:26:01 +0200

    Change constants imports and exports

--- frontend/src/routes/register.js
+++ frontend/src/routes/register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import USERSSERVICE from "../constants";
+import {USERSSERVICE} from "../constants";
 
 const PASSWORD_MAX_LENGTH = 20;
 const PASSWORD_MIN_LENGTH = 5;
