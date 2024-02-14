31bc40118ecae1ef4826e31d0f32be0380c4bc3f
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2024-02-14 22:22:24 +0200

    Remove redundant comment

--- frontend/src/pages/infoPages/tac.jsx
+++ frontend/src/pages/infoPages/tac.jsx
@@ -3,33 +3,6 @@ import { Container, Card } from "react-bootstrap";
 import InfoCustomizableTextComponent from "./infoComponent";
 import BackButton from "../components/BackButton";
 
-/* export default function tac() {
-    return (
-        <div>
-            <Container>
-                <Row className="vh-10 d-flex justify-content-center align-items-center">
-                    <Col md={8} lg={10} xs={8}>
-                        <Card>
-                            <Card.Body>
-                                <div className="mb-3 mt-md-4">
-                                    <BackButton navigateTo="/login" /> <br />
-                                    <div>
-                                        <InfoCustomizableTextComponent
-                                            headerText="Terms and Conditions"
-                                            paragraphText="Lorem iorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-                                        />
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-} */
-
 export default function tac() {
     return (
         <div
