78e258f4364ce421e59fac6069b830914ec27ac7
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2024-02-27 01:45:44 +0200

    Add labels, time and date

--- frontend/src/pages/components/EventCard.jsx
+++ frontend/src/pages/components/EventCard.jsx
@@ -1,15 +1,62 @@
 import React from "react";
 import "./EventCard.css";
+import { useState } from "react";
+
 
 const EventCard = ({eventData}) => {
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [labels, setLabels] = useState("");
+
+    // Get date and time.
+    if ( (date === "" || time === "") && eventData.date != null ) {
+        const newDate = new Date(eventData.date);
+        const dateString = newDate.toLocaleDateString("fi-FI");
+        
+        let hours = newDate.getHours();
+        if ( hours < 10)
+            hours = "0" + hours
+        let minutes = newDate.getMinutes();
+        if ( minutes < 10)
+            minutes = "0" + minutes
+
+        const timeString = hours + "." + minutes;
+        setTime(timeString);
+        setDate(dateString);
+    }
+
+    // Get tags and categories.
+    if ( labels === "" )  {
+        const tagsElements = eventData.tags?.map(
+            (tag) => <li className="event-tag" key={tag}>{tag.substring(0, (Math.floor(Math.random() * 5) + 5))}</li>
+        
+        )
+        const categoriesElements = eventData.tags?.map(
+            (category) => <li className="event-category" key={category}>{category.substring(0, 6)}</li>
+        )
+        const labelsElements = [...(categoriesElements ?? []), ...(tagsElements ?? [])];
+        if ( labelsElements.length > 0 ) {
+            const labelsList = <ul>{labelsElements}</ul>
+            setLabels(labelsList)
+        }
+    }
 
     return (
-        <div class="card-background">
-            <div class="card-image" >
+        <div className="card-background">
+            <div className="card-image" >
                 <img src={eventData.imageURL} alt=""/>
             </div>
-            <p class="event-name">{eventData.eventName}</p>
-            <p class="event-host">Hosted by {eventData.host}</p>
+            <p className="event-name">{eventData.eventName}</p>
+            <p className="event-host">Hosted by {eventData.host}</p>
+            <p className="event-date">
+                <i className="bi bi-calendar-event"></i>
+                {date}
+            </p>
+            <p className="event-time">
+                <i className="bi bi-clock"></i>
+                {time}
+            </p>
+            <div className="event-labels">{labels}</div>
         </div>
     );
 }
